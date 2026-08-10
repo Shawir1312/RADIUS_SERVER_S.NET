4de993ce99f4f7799d3871a5d4b3c1942e9384cd
Mushawir Odegoa <mushawirodegoa@MacBook-Air-Mushawir.local>
2026-08-11 02:00:49 +0900

    Fix: Add missing functions.php include in get_resellers.php API

--- process/get_resellers.php
+++ process/get_resellers.php
@@ -5,6 +5,7 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../config/auth.php';
+require_once __DIR__ . '/../include/functions.php';
 
 auth_check();
 
