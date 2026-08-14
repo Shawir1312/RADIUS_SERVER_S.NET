e85781ce2b63266a141c2d08ea74239601d96d2f
Mushawir Odegoa <mushawirodegoa@MacBook-Air-Mushawir.local>
2026-08-15 02:10:44 +0900

    Fix: Tambahkan require config.php untuk auth

--- run_migration.php
+++ run_migration.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/config/config.php';
 require_once __DIR__ . '/config/database.php';
 
 // Cek apakah admin sudah login (Keamanan agar tidak sembarang orang bisa mengeksekusi)
